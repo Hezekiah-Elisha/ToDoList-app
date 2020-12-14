9ed63efeadf7e3227e85caeadf0c6a5bbcb09da7
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2020-12-15 02:13:10 +0300

    fourth commit

--- app/src/main/java/com/maxtell/todolist/MainActivity.kt
+++ app/src/main/java/com/maxtell/todolist/MainActivity.kt
@@ -4,18 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.maxtell.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var recyclerAdapter: TodoItemsAdapter
+    private lateinit var recyclerLayoutManager: RecyclerView.LayoutManager
+
+    var todoItemsList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        todoItemsList.add("Buy groceries")
+        todoItemsList.add("Do laundry")
+        todoItemsList.add("Play guitar")
+
+        recyclerLayoutManager = LinearLayoutManager(this)
+        recyclerAdapter = TodoItemsAdapter(todoItemsList)
+
+        binding.todoItemRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = recyclerLayoutManager
+            adapter = recyclerAdapter
+        }
 
     }
     public fun navToAddItemAction(view: View){
--- app/src/main/java/com/maxtell/todolist/TodoItemsAdapter.kt
+++ app/src/main/java/com/maxtell/todolist/TodoItemsAdapter.kt
@@ -1,4 +1,28 @@
 package com.maxtell.todolist
 
-class TodoItemsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoItemsAdapter(val todoItemsList:ArrayList<String>):
+    RecyclerView.Adapter<TodoItemsAdapter.viewHolder>() {
+    class viewHolder(val constraintLayout:ConstraintLayout):RecyclerView.ViewHolder(constraintLayout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val constraintLayout = LayoutInflater.from(parent.context).inflate(R.layout.to_do_item_layout,parent,false) as ConstraintLayout
+
+        return viewHolder(constraintLayout)
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val constraintLayout = holder.constraintLayout
+        val nameTextView = constraintLayout.getChildAt(0) as TextView
+        nameTextView.text = todoItemsList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return todoItemsList.size
+    }
 }
